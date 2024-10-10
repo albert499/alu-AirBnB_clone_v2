3d12bc892d5e1d285f427631565b9137c81c74e6
alberta logozaga <a.logozaga@alustudent.com>
2024-10-10 13:50:18 +0000

    state.py

--- models/state.py
+++ models/state.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-"""This file defines the State Model
-It inherits from the BaseModel
-"""
 
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-from models.base_model import BaseModel
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
-
-class State(BaseModel):
-    """The State Model"""
-
-    # Attributes
-    name = ""
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
